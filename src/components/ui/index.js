98a0a74950112f0b9f913c77b2000d3b814c3468
EPSO <gmaiarviana@gmail.com>
2025-08-06 14:30:13 -0300

    feat: remover arquivos design system complexo desnecessário - Funcionalidade Simplificação CONCLUÍDA

--- src/components/ui/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Button } from './Button.jsx';
-export { default as Input } from './Input.jsx';
-export { default as Card, CardHeader, CardContent, CardFooter } from './Card.jsx';
-export { default as Container } from './Container.jsx';
-export { default as Header } from './Header.jsx';
-export { default as FormGroup } from './FormGroup.jsx';
